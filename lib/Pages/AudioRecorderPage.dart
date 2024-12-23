4d4adbb83de6e94e2268b3151a0958a01d3c2917
khadija <khadija.abdelilah4@gmail.com>
2024-12-23 11:03:34 +0100

    commit2

--- lib/Pages/AudioRecorderPage.dart
+++ lib/Pages/AudioRecorderPage.dart
@@ -1,8 +1,7 @@
-import 'package:ai_deploy/Services/gemini_service.dart';
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
-
+import 'package:ai_deploy/Services/gemini_service.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Reconnaissance Vocale avec Gemini',
+      debugShowCheckedModeBanner: false,
+      title: 'Assistant Vocal avec Gemini',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
@@ -23,185 +23,253 @@ class MyApp extends StatelessWidget {
 
 class AudioRecorderPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _AudioRecorderPageState createState() => _AudioRecorderPageState();
 }
 
-class _MyHomePageState extends State<AudioRecorderPage> {
+class _AudioRecorderPageState extends State<AudioRecorderPage> with SingleTickerProviderStateMixin {
   final SpeechToText _speechToText = SpeechToText();
-  final GeminiService _geminiService = GeminiService('AIzaSyBTjoqC3-CU-hMJonWsADVRP9gOC9qwelw'); // Remplacez par votre clé API
+  final GeminiService _geminiService = GeminiService('AIzaSyBTjoqC3-CU-hMJonWsADVRP9gOC9qwelw');
   bool _speechEnabled = false;
+  bool _isListening = false;
   String _lastWords = '';
-  String _geminiResponse = '';
+  List<Map<String, String>> _conversationHistory = [];
+  List<String> _currentConversation = [];
+  late AnimationController _animationController;
 
   @override
   void initState() {
     super.initState();
     _initSpeech();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat(reverse: true);
   }
 
-  /// Initialiser la reconnaissance vocale
   void _initSpeech() async {
-    bool hasPermission = await _speechToText.initialize();
-    setState(() {
-      _speechEnabled = hasPermission;
-    });
+    bool hasPermission = await _speechToText.initialize(
+      onStatus: (status) => print('Status: $status'),
+      onError: (error) => print('Error: $error'),
+    );
 
     if (!hasPermission) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Permission du microphone refusée.')),
+        SnackBar(content: Text('Microphone permission denied.')),
       );
     }
+
+    setState(() {
+      _speechEnabled = hasPermission;
+    });
   }
 
-  /// Commencer l'écoute avec vérification des permissions
   void _startListening() async {
     if (_speechEnabled && !_speechToText.isListening) {
       await _speechToText.listen(onResult: _onSpeechResult);
-      setState(() {});
-    } else if (!_speechEnabled) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('La reconnaissance vocale n\'est pas disponible.')),
-      );
+      setState(() {
+        _isListening = true;
+      });
+      _animationController.repeat();
     }
   }
 
-  /// Arrêter l'écoute et envoyer le texte à Gemini
   void _stopListening() async {
     await _speechToText.stop();
-    _sendToGemini(_lastWords); // Envoyer le texte reconnu à Gemini
-    setState(() {});
+    setState(() {
+      _isListening = false;
+    });
+    _animationController.stop();
+    _sendToGemini(_lastWords);
   }
 
-  /// Gestion des résultats de la reconnaissance vocale
   void _onSpeechResult(SpeechRecognitionResult result) {
     setState(() {
       _lastWords = result.recognizedWords;
     });
   }
 
-  /// Envoyer le texte reconnu à Gemini pour générer une réponse
   void _sendToGemini(String prompt) async {
-    if (prompt.isEmpty) {
+    if (prompt.isEmpty) return;
+
+    setState(() {
+      _currentConversation.add('You: $prompt');
+      _currentConversation.add('Génération de la réponse...');
+    });
+
+    try {
+      final response = await _geminiService.generateResponse(prompt);
       setState(() {
-        _geminiResponse = 'Aucun texte à envoyer.';
+        _currentConversation[_currentConversation.length - 1] = 'Gemini: $response';
       });
-      return;
+    } catch (e) {
+      setState(() {
+        _currentConversation[_currentConversation.length - 1] =
+        'Erreur lors de la génération de la réponse.';
+      });
     }
+  }
 
+  void _saveConversation() {
     setState(() {
-      _geminiResponse = 'Génération de la réponse...';
+      _conversationHistory.add({
+        'title': 'Conversation ${_conversationHistory.length + 1}',
+        'content': _currentConversation.join('\n'),
+      });
+      _currentConversation.clear();
     });
+  }
 
-    final response = await _geminiService.generateResponse(prompt);
+  void _loadConversation(int index) {
     setState(() {
-      _geminiResponse = response;
+      _currentConversation = _conversationHistory[index]['content']!.split('\n');
     });
+  }
+
+  @override
+  void dispose() {
+    _speechToText.stop();
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Assistant Vocal avec Gemini'),
+        title: Text('Assistant Vocal avec Gemini', style: TextStyle(fontSize: 22)),
+        backgroundColor: Colors.deepPurple,
+        elevation: 0,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.deepPurple, Colors.deepPurpleAccent],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  'Texte reconnu :',
-                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.deepPurple, Colors.purple],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
               ),
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(16),
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                        offset: Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: Center(
-                    child: Text(
-                      _speechToText.isListening
-                          ? _lastWords
-                          : _speechEnabled
-                          ? 'Appuyez sur "Start" pour commencer à écouter...'
-                          : 'La reconnaissance vocale n\'est pas disponible.',
-                      style: TextStyle(fontSize: 16),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-              ),
-              // Zone pour afficher la réponse de Gemini
-              if (_geminiResponse.isNotEmpty)
-                Container(
-                  margin: EdgeInsets.all(16),
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Text(
-                    'Réponse Gemini : $_geminiResponse',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-              // Boutons Start et Stop
-              Row(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    onPressed: _speechToText.isListening ? null : _startListening,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                    ),
-                    child: Text(
-                      "Start",
-                      style: TextStyle(fontSize: 16, color: Colors.white),
-                    ),
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: AssetImage('assets/images/avatar.png'),
                   ),
-                  SizedBox(width: 20),
-                  ElevatedButton(
-                    onPressed: _speechToText.isListening ? _stopListening : null,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                    ),
-                    child: Text(
-                      "Stop",
-                      style: TextStyle(fontSize: 16, color: Colors.white),
+                  SizedBox(height: 10),
+                  Text(
+                    'Historique des Conversations',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 20),
-            ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _conversationHistory.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Icon(Icons.history, color: Colors.deepPurple),
+                    title: Text(
+                      _conversationHistory[index]['title']!,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    onTap: () => _loadConversation(index),
+                  );
+                },
+              ),
+            ),
+            if (_conversationHistory.isEmpty)
+              ListTile(
+                title: Text('Aucune conversation enregistrée'),
+              ),
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF6A1B9A), Color(0xFF4A148C)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.all(16),
+                itemCount: _currentConversation.length,
+                itemBuilder: (context, index) {
+                  final isUserMessage = _currentConversation[index].startsWith('You:');
+                  return _buildChatBubble(_currentConversation[index], isUserMessage);
+                },
+              ),
+            ),
+            SizedBox(height: 10), // Add spacing before the buttons
+            Center(
+              child: AnimatedBuilder(
+                animation: _animationController,
+                builder: (context, child) {
+                  return Transform.scale(
+                    scale: 1.0 + 0.05 * _animationController.value,
+                    child: FloatingActionButton(
+                      onPressed: _isListening ? _stopListening : _startListening,
+                      backgroundColor: _isListening ? Colors.red : Colors.deepPurple,
+                      child: Icon(
+                        _isListening ? Icons.stop : Icons.mic,
+                        size: 32,
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ElevatedButton(
+                onPressed: _saveConversation,
+                child: Text('Enregistrer la Conversation'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  foregroundColor: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildChatBubble(String message, bool isUser) {
+    return Align(
+      alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        padding: EdgeInsets.all(16),
+        constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.75),
+        decoration: BoxDecoration(
+          color: isUser ? Colors.blueAccent : Colors.grey[900],
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomLeft: isUser ? Radius.circular(20) : Radius.zero,
+            bottomRight: isUser ? Radius.zero : Radius.circular(20),
+          ),
+        ),
+        child: Text(
+          message,
+          style: TextStyle(color: Colors.white, fontSize: 16),
         ),
       ),
     );
--- lib/Pages/Cnn.dart
+++ lib/Pages/Cnn.dart
@@ -1,18 +1,166 @@
-import 'package:flutter/material.dart';
+// File: lib/Pages/CnnPage.dart
 
-class CnnPage extends StatelessWidget {
-  const CnnPage({super.key});
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CnnPage extends StatefulWidget {
+  const CnnPage({Key? key}) : super(key: key);
+
+  @override
+  _CnnPageState createState() => _CnnPageState();
+}
+
+class _CnnPageState extends State<CnnPage> {
+  File? _imageFile;
+  String? _prediction;
+  bool _isLoading = false;
+
+  final ImagePicker _picker = ImagePicker();
+
+  // Fonction pour sélectionner une image depuis la galerie ou la caméra
+  Future<void> _pickImage(ImageSource source) async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(
+        source: source,
+        imageQuality: 80, // Compression de l'image
+      );
+
+      if (pickedFile != null) {
+        setState(() {
+          _imageFile = File(pickedFile.path);
+          _prediction = null; // Réinitialiser la prédiction
+        });
+      }
+    } catch (e) {
+      print('Erreur lors de la sélection de l\'image: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erreur lors de la sélection de l\'image: $e')),
+      );
+    }
+  }
+
+  // Fonction pour afficher le dialogue de sélection de source d'image
+  void _showImageSourceSelection() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Galerie'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  _pickImage(ImageSource.gallery);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo_camera),
+                title: const Text('Caméra'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  _pickImage(ImageSource.camera);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Fonction pour obtenir la prédiction du modèle CNN
+  Future<void> _getPrediction() async {
+    if (_imageFile == null) return;
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      // TODO: Intégrer votre logique de prédiction ici
+      // Exemple de simulation de prédiction
+      await Future.delayed(Duration(seconds: 2)); // Simuler un délai de traitement
+      setState(() {
+        _prediction = "Dog"; // Remplacez par la prédiction réelle
+      });
+    } catch (e) {
+      print('Erreur lors de la prédiction: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erreur lors de la prédiction: $e')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CNN Page'),
+        title: const Text('Classification CNN'),
+        backgroundColor: Colors.indigo,
       ),
-      body: Center(
-        child: const Text(
-          'This is the CNN Page.',
-          style: TextStyle(fontSize: 24),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            const Text(
+              "CLASSIFICATION D'IMAGE",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton.icon(
+              onPressed: _showImageSourceSelection,
+              icon: const Icon(Icons.upload),
+              label: const Text('Télécharger une Image'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(50),
+              ),
+            ),
+            const SizedBox(height: 20),
+            _imageFile != null
+                ? Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Image.file(
+                      _imageFile!,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  _isLoading
+                      ? const CircularProgressIndicator()
+                      : ElevatedButton(
+                    onPressed: _getPrediction,
+                    child: const Text('Classer l\'Image'),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  _prediction != null
+                      ? Text(
+                    'Prédiction: $_prediction',
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                      : Container(),
+                ],
+              ),
+            )
+                : Container(),
+          ],
         ),
       ),
     );
